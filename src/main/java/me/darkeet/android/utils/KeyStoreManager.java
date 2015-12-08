5242ef4b849e6f9d61d58ecb7b05bc501f99d341
selience <loveselience@gmail.com>
2015-12-08 16:36:44 +0800

    add encodeutils file

---
+++ src/main/java/me/darkeet/android/utils/KeyStoreManager.java
@@ -0,0 +1,151 @@
+package me.darkeet.android.utils;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.security.KeyPairGeneratorSpec;
+import android.util.Base64;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Enumeration;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.security.auth.x500.X500Principal;
+
+/**
+ * Name: KeyStoreManager
+ * User: Lee (darkeet.me@gmail.com)
+ * Date: 2015/12/7 18:17
+ * Desc: KeyStore存放安全数据方法
+ * https://github.com/bboyfeiyu/android-tech-frontier/blob/master/issue-29/%E5%BC%80%E5%8F%91%E5%AE%89%E5%85%A8%E7%9A%84Android%E5%BA%94%E7%94%A8.md
+ */
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class KeyStoreManager {
+
+    private KeyStore keyStore;
+
+    private static class SingletonHolder {
+
+        private static KeyStoreManager instance = new KeyStoreManager();
+    }
+
+    private KeyStoreManager() {
+        initKeyStore();
+    }
+
+    private void initKeyStore() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+        } catch (Exception e) {
+        }
+    }
+
+    public ArrayList<String> refreshKeys() {
+        ArrayList<String> keyAliases = new ArrayList<>();
+        try {
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                keyAliases.add(aliases.nextElement());
+            }
+        } catch (Exception e) {
+        }
+
+        return keyAliases;
+    }
+
+    public void createNewKeys(Context context, String alias) {
+        try {
+            // Create new key if needed
+            if (!keyStore.containsAlias(alias)) {
+                Calendar start = Calendar.getInstance();
+                Calendar end = Calendar.getInstance();
+                end.add(Calendar.YEAR, 1);
+                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(context)
+                        .setAlias(alias)
+                        .setSubject(new X500Principal("CN=Sample Name, O=Android Authority"))
+                        .setSerialNumber(BigInteger.ONE)
+                        .setStartDate(start.getTime())
+                        .setEndDate(end.getTime())
+                        .build();
+                KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
+                generator.initialize(spec);
+
+                KeyPair keyPair = generator.generateKeyPair();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void deleteKey(final String alias) {
+        try {
+            keyStore.deleteEntry(alias);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String encryptString(String alias, String initialText) {
+        try {
+            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(alias, null);
+            RSAPublicKey publicKey = (RSAPublicKey) privateKeyEntry.getCertificate().getPublicKey();
+
+            Cipher inCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding", "AndroidOpenSSL");
+            inCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            CipherOutputStream cipherOutputStream = new CipherOutputStream(
+                    outputStream, inCipher);
+            cipherOutputStream.write(initialText.getBytes("UTF-8"));
+            cipherOutputStream.close();
+
+            byte[] vals = outputStream.toByteArray();
+            return Base64.encodeToString(vals, Base64.DEFAULT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String decryptString(String alias, String encrypted) {
+        try {
+            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(alias, null);
+            //RSAPrivateKey privateKey = (RSAPrivateKey) privateKeyEntry.getPrivateKey();
+
+            Cipher output = Cipher.getInstance("RSA/ECB/PKCS1Padding", "AndroidOpenSSL");
+            output.init(Cipher.DECRYPT_MODE, privateKeyEntry.getPrivateKey());
+
+            CipherInputStream cipherInputStream = new CipherInputStream(
+                    new ByteArrayInputStream(Base64.decode(encrypted, Base64.DEFAULT)), output);
+            ArrayList<Byte> values = new ArrayList<>();
+            int nextByte;
+            while ((nextByte = cipherInputStream.read()) != -1) {
+                values.add((byte) nextByte);
+            }
+
+            byte[] bytes = new byte[values.size()];
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = values.get(i).byteValue();
+            }
+
+            return new String(bytes, 0, bytes.length, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
